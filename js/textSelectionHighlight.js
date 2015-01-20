8cd7a95919b440fc04cbbb756d3279e43e1b258b
404pnf <404pnf@exmaple.org>
2015-01-20 13:36:35 +0800

    用rangy.js实现了正确的行为

--- js/textSelectionHighlight.js
+++ js/textSelectionHighlight.js
@@ -6,29 +6,54 @@
 // 1. 用户选择id为yuedu的div中某些文字，会为选中的文字添加高亮样式。
 // 2. 用户点击已经高亮的文字，会取消高亮样式。
 (function () {
+
+    rangy.init();
+    // 正确的实现
+    $('#yuedu').mouseup(function(e) {
+        document.getSelection();
+        rangy.createCssClassApplier('annotation').applyToSelection();
+    });
+
     // ## 这个实现也是错误的。
     // 它比下面那个错误好一点的地方时用正则替换了所有匹配选中的部分。
     // 比如  abc def abc hjk
     // 如果选择了第二个abc，会高亮第一个和第二个abc，虽然也不好，
     // 但至少用户不会觉得自己选中的没有高亮而是去高亮一个不相关的地方，
     //
-    // ## 选中部分可以改进的。
-    // 现在用户选择什么就是什么，哪怕用户选择一个单词当中的一个字母。
-    // 可以改进为最小选择区域是一个单词。即使用户选择了一个字母，也扩展为字母所在的单词。
+    // ## 改进选中部分。
+    // 即使用户选择了一个字母，也扩展为字母所在的单词。
     // 这个行为和kindle保持一致。
-    // 实现方式未想到。
+    // 使用方法，正则：
+    // 捕获 零个或多个非空白且非'>'字符 加鼠标选中的字符 加 零个或多个非空白且非'<'或'>'字符
+    // 中的鼠标选中字符。
+    /*
     $('#yuedu').mouseup(function(e) {
         var content, re, replacement, newHTML, oldHTML, target;
-        content = window.getSelection().toString();
-        re = new RegExp(content,"g"); // 为了在正则中用变量，只能用regexp构造方法
-        replacement = "<span class='annotation'>" + content + "</span>"
+
+        { try{
+            // non-IE
+            content = document.getSelection().toString();
+        } catch (err) {
+            // IE
+            content = document.selection.createRange().text;
+            }
+        }
+        // 1. 为了在正则中用变量，只能用regexp构造方法。
+        // 2. 捕获content的括号分散在前后两个正则中，在js的正则使用变量只能这样。脏乱差啊。
+        // ruby中很干净啦 Regexp.new(" (#{a}) ")
+        // 3. content前面去匹配html的结束符号'>'，后面是去匹配hmtl标签的开始符号'<'。
+        re = new RegExp('([^ >]*' + content + '[^ <]*)', "g");
         if (content !== '') {
             target = e.target;
             oldHTML = target.innerHTML;
-            newHTML = oldHTML.replace(re, replacement);
+            newHTML = oldHTML.replace(re, function (sel) {
+                console.log(sel);
+                return "<span class='annotation'>" + sel + "</span>"
+            });
             return target.innerHTML = newHTML;
         }
     });
+    */
 
     // ## 我写了取消高亮的功能
     // 下面的实现是做不到的。
@@ -38,13 +63,21 @@
     // 如果选择了第二个abc，会高亮第一个和第二个abc，虽然也不好，
     // 但如果点击第二个abc触发取消高亮，第一个abc不会关联也取消的。
     // 这是正确的方式。只是由于实现高亮用了错误的方式，才让这个取消功能也变得有点怪。
-    // ### 现在取消高亮的实现方式还存在一个问题，
-    // 就是如果选取的高亮只有一两个字符，点击取消的时候，会出现没有取消，反而高亮了更多的文字。
-    // 产生原因是用户觉得自己只是点了一下鼠标，实际dom检测到的是点下鼠标的时候稍微移动了一下。
-    // 解决方法暂时没想出来。
+
+    // 取消高亮的时候，我们不但要取消css类，也应该取消span。否则遗留在页面的span会有时让
+    // 页面混乱。还好，有jquery帮忙。
     $('#yuedu').delegate("span", "click", function (e) {
+        var text = $(this).text();
         console.log('triggered!');
-        $(this).toggleClass('annotation');
+        $(this).replaceWith(text);
+    });
+    // 需要有这个事件监听。它会在用户试图选中已经高亮的区域时先取消掉高亮。
+    // 如果不这样做。用户再次选择已经高亮区域的部分文字，会出现嵌套的span。
+    // 在某些情况下让页面异常混乱。
+    $('#yuedu').delegate("span", "mousedown", function (e) {
+        var text = $(this).text();
+        console.log('triggered!');
+        $(this).replaceWith(text);
     });
 })();
 
@@ -53,15 +86,18 @@
 // 这个实现是错误的，简单的正则替换，会替换文章中第一次出现的选中词，而不是选中词。
 // 比如  abc def abc hjk
 // 如果选择了第二个abc，会高亮第一个abc，而第二个abc根本没变化。非常让人疑惑
-// document.onmouseup=function(e){
-//     content=window.getSelection().toString();
-//     if(content!=''){
-//         var len=content.length;
-//         var   target=e.target;
-//         var  position=target.innerHTML.indexOf(content);
-//         var  position2=position+len;
-//         var  tempstr1=target.innerHTML.substring(0,position);
-//         var tempstr2=target.innerHTML.substring(position2);
-//         content="<span style='color:red;'>"+content+"</span>";
-//         target.innerHTML=tempstr1+content+tempstr2;
-//     }}
+//
+// 而且这个例子的代码也真是脏乱差啊。
+//
+            // document.onmouseup=function(e){
+            //     content=window.getSelection().toString();
+            //     if(content!=''){
+            //         var len=content.length;
+            //         var   target=e.target;
+            //         var  position=target.innerHTML.indexOf(content);
+            //         var  position2=position+len;
+            //         var  tempstr1=target.innerHTML.substring(0,position);
+            //         var tempstr2=target.innerHTML.substring(position2);
+            //         content="<span style='color:red;'>"+content+"</span>";
+            //         target.innerHTML=tempstr1+content+tempstr2;
+            //     }}
