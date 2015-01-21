2c48ddd20fd7b84bb1f6a978b0c616484814c237
404pnf <404pnf@exmaple.org>
2015-01-21 10:34:35 +0800

    合并两个delegate函数 .delegat(tag, 'evt1 evt2', cb)

--- js/textSelectionHighlight.js
+++ js/textSelectionHighlight.js
@@ -20,15 +20,13 @@
     //
     // 取消高亮的时候，我们不但要取消css类，也应该取消span。否则遗留在页面的span会有时让
     // 页面混乱。还好，有jquery帮忙。
-    $('#yuedu').delegate("span", "click", function (e) {
-        var text = $(this).text();
-        $(this).replaceWith(text);
-    });
-    // 需要有这个事件监听。它会在用户试图选中已经高亮的区域时先取消掉高亮。
+    //
+    // 需要click和mousedown两个事件监听。它会在用户试图选中已经高亮的区域时先取消掉高亮。
     // 如果不这样做。用户再次选择已经高亮区域的部分文字，会出现嵌套的span。
     // 在某些情况下让页面异常混乱。
-    $('#yuedu').delegate("span", "mousedown", function (e) {
+    $('#yuedu').delegate("span", "click mousedown", function (e) {
         var text = $(this).text();
         $(this).replaceWith(text);
     });
+
 })();
